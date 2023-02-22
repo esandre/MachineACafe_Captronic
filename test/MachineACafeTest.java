34763046c94ca3cd72d52f47814e11e0894b52db
Nicolas Touchard <ntouchard@nke.fr>
2023-02-22 10:22:55 +0100

    refacto insert en centimes

--- test/MachineACafeTest.java
+++ test/MachineACafeTest.java
@@ -9,12 +9,12 @@ public class MachineACafeTest {
         //Etant donné
         MachineACafe machine = new MachineACafe();
         int nbCafe = machine.GetNbCafe();
-        //Quand l’utilisateur met somme >= 0.40
-        machine.Insert( 0.40 );
+        //Quand l’utilisateur met somme >= 40 centimes
+        machine.Insert( 40 );
 
         //Alors somme encaissée
         double somme = machine.GetSomme();
-        assertEquals( 0.40, somme, 0.05 );
+        assertEquals( 0.4, somme, 0.05 );
         //Et nb cafés augmente de 1
         assertEquals( nbCafe + 1, machine.GetNbCafe() );
     }
@@ -25,8 +25,8 @@ public class MachineACafeTest {
 
         //Etant donné pas de café sans café
         MachineACafe machine = new MachineACafe(0);
-        //Quand l’utilisateur met somme >= 0.40
-        machine.Insert( 0.40 );
+        //Quand l’utilisateur met somme >= 40 centimes
+        machine.Insert( 40 );
 
         //Alors retourne somme
         double somme = machine.GetSomme();
