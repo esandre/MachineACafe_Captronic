f6e4a6505de5fde2474102bd3202486ae232e609
Nidal <nbedyouch@kreon3d.com>
2023-02-22 09:55:30 +0100

    test first use case

--- src/MachineACafe.java
+++ src/MachineACafe.java
@@ -1,2 +1,15 @@
 public class MachineACafe {
+
+    private int servedCafe = 0;
+    public int GetNbCafe() {
+       return servedCafe++;
+    }
+
+    public void Insert(double v) {
+
+    }
+
+    public double GetSomme() {
+        return 0.40;
+    }
 }
--- test/MachineACafeTest.java
+++ test/MachineACafeTest.java
@@ -1,8 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MachineACafeTest {
     @Test
-    public void Test_Env(){
+    public void Test_Env()
+    {
+        //Etant donné
         MachineACafe machine = new MachineACafe();
+        int nbCafe = machine.GetNbCafe();
+        //Quand l’utilisateur met somme >= 0.40
+        machine.Insert( 0.40 );
+
+        //Alors somme encaissée
+        double somme = machine.GetSomme();
+        assertEquals( 0.40, somme, 0.05 );
+        //Et nb cafés augmente de 1
+        assertEquals( nbCafe + 1, machine.GetNbCafe() );
     }
 }
