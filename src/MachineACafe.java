9edc4e92e46db15fcb8a7b42295fc81566a0e854
Enzo Sandré <atelier@enzosandre.fr>
2023-02-22 09:44:35 +0100

    Création d'une machine à café vide

---
+++ src/MachineACafe.java
@@ -0,0 +1,2 @@
+public class MachineACafe {
+}
--- test/MachineACafeTest.java
+++ test/MachineACafeTest.java
@@ -3,5 +3,6 @@ import org.junit.jupiter.api.Test;
 public class MachineACafeTest {
     @Test
     public void Test_Env(){
+        MachineACafe machine = new MachineACafe();
     }
 }
