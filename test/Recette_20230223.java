eedd221d64293ed39a5caa3b095be69748bc9165
Enzo Sandré <atelier@enzosandre.fr>
2023-02-23 11:22:31 +0100

    Test de recette de la démo de ce matin

---
+++ test/Recette_20230223.java
@@ -0,0 +1,50 @@
+import machine.MachineACafe;
+import org.junit.Ignore;
+import org.junit.Test;
+import utilities.MachineACafeBuilder;
+
+import static utilities.MachineACafeMatchers.assertThat;
+
+public class Recette_20230223 {
+    @Test
+    @Ignore("Test de recette")
+    public void Scenario1(){
+        var machine = new MachineACafeBuilder().Build();
+
+        // Café allongé
+        {
+            var nombreCafésInitiaux = machine.GetNbCafe();
+            var consommationEauInitiale = machine.GetConsommationEau();
+            machine.pressCafeLong();
+            machine.Insérer(MachineACafe.PrixDuCaféEnCentimes);
+
+            assertThat(machine).sertUnCaféLong(nombreCafésInitiaux, consommationEauInitiale);
+        }
+
+        // Café court sucré
+        {
+            var nombreCafésInitiaux = machine.GetNbCafe();
+            var stockSucreInitial = machine.GetStockSucre();
+            var consommationEauInitiale = machine.GetConsommationEau();
+
+            machine.DemanderSucre();
+            machine.Insérer(MachineACafe.PrixDuCaféEnCentimes);
+
+            assertThat(machine).sertUnCaféCourtSucré(nombreCafésInitiaux, stockSucreInitial, consommationEauInitiale);
+        }
+
+
+        // Café allongé sucré
+        {
+            var nombreCafésInitiaux = machine.GetNbCafe();
+            var stockSucreInitial = machine.GetStockSucre();
+            var consommationEauInitiale = machine.GetConsommationEau();
+
+            machine.DemanderSucre();
+            machine.pressCafeLong();
+            machine.Insérer(MachineACafe.PrixDuCaféEnCentimes);
+
+            assertThat(machine).sertUnCaféLongSucré(nombreCafésInitiaux, stockSucreInitial, consommationEauInitiale);
+        }
+    }
+}
